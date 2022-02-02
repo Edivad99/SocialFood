b1965f0482ccdbe7da31fdf603b21d076e1e87bf
Davide Albiero <davide2910@live.it>
2022-02-02 18:40:57 +0100

    Sha password

---
+++ SocialFood.Web/Utils/SHAUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SocialFood.Web.Utils;
+
+public class SHAUtil
+{
+    public static String CalculateHash(string value)
+    {
+        using SHA256 hash = SHA256.Create();
+        return String.Concat(hash
+          .ComputeHash(Encoding.UTF8.GetBytes(value))
+          .Select(item => item.ToString("x2")));
+    }
+}
+
